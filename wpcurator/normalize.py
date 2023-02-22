98a8ee29b55a2a64262440b72e7adb4af18a0c64
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2023-02-22 18:05:35 +0100

    ignores and slighly more crating

--- wpcurator/normalize.py
+++ wpcurator/normalize.py
@@ -1,6 +1,24 @@
 def validated_workflowhub_rocrate(url, label):
     import time
 
+    from rocrate.rocrate import ROCrate
+    from rocrate.model.person import Person
+
+    crate = ROCrate()
+    wfl = crate.add_workflow(url)
+    
+    vs_id = "https://orcid.org/0000-0001-6353-0808"    
+    vs = crate.add(Person(crate, vs_id, properties={
+        "name": "Volodymyr Savchenko",
+        "affiliation": "EPFL, UNIGE"
+    }))
+
+    wfl["author"] = vs
+
+    # TODO: upload somewhere
+    # TODO: upload/publish functionality in rocrate?
+    crate.write("exp_crate")
+
     return (
         f"\\href{{{url}}}{{{label}}}{{\\color{{green}} \\circledmarki}}"        
         "\\textit{\\footnotesize ("
